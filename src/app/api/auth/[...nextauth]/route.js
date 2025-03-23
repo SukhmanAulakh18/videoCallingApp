0bc607ebc5051f6078be6bbbf02c2146234312aa
SukhmanAulakh18 <ausukhma@sheridancollege.ca>
2025-03-23 08:19:39 -0400

    fixed auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -12,52 +12,59 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
-  callbacks:{
-    async jwt({token,user,account}){
-
-        if(user){
-            token.id = user.id;
-        }
-        if(account){
-            token.accessToken= account.access_token;
-        }
-
-        return token;
-    },
-    async session({ session, token }) {
-         session.user.id= token.id;
-        return session
-      },
-
-      async signIn({user,profile}) {
-        await dbConnect();
-        let dbUser = await User.findOne({email: user.email})
-
-        //if user not found create a new user
-        if(!dbUser){
-            dbUser = await User.create({
-                name:profile.name,
-                email:profile.email,
-                profilePicture:profile.picture,
-                isVerified:profile.email_verified ? true :false
-            })
-        }
-         user.id = dbUser._id.toString();
-         return true;
+  callbacks: {
+    // JWT callback to include user ID and access token in the token
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.picture = user.image;
       }
-  },
-  session:{
-    strategy: 'jwt',
-    maxAge : 90 * 24 * 60 * 60
-  },
-  pages:{
-    signIn: '/user-auth',
-  }
-}
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    // Session callback to include user ID from token
+    async session({ session, token }) {
+      session.user.id = token.id;
+      session.user.email = token.email;
+      session.user.name = token.name;
+      session.user.image = token.picture;
+      return session;
+    },
+    // Sign-in callback to check if the user exists or create a new one
+    async signIn({ user, profile }) {
+      await dbConnect();
+      let dbUser = await User.findOne({ email: user.email });
 
-const handle = NextAuth(authOptions)
-export {handle as POST , handle as GET};
+      // If user not found, create a new user
+      if (!dbUser) {
+        dbUser = await User.create({
+          name: user.name || profile.name,
+          email: user.email,
+          profilePicture: user.image || profile.picture,
+          isVerified: profile.email_verified || false,
+        });
+      }
+      // Assign dbUser._id to user object
+      user.id = dbUser._id.toString();
+      return true;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 90 * 24 * 60 * 60, // 90 days
+  },
+  pages: {
+    signIn: '/user-auth',
+  },
+};
+
+const handle = NextAuth(authOptions);
+export { handle as POST, handle as GET };
